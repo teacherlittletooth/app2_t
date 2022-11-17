eb52be534213eeb2ef45a3d30e5db95ba022e122
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2022-11-16 21:59:50 -0300

    Update main.dart

--- Aula 9/main.dart
+++ Aula 9/main.dart
@@ -5,27 +5,18 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Tela de login",
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange
-      ),
-      home: const MyLogin()
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Login",
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        home: const MyLogin());
   }
 }
-
 
 class MyLogin extends StatefulWidget {
   const MyLogin({Key? key}) : super(key: key);
@@ -37,94 +28,113 @@ class MyLogin extends StatefulWidget {
 class _MyLoginState extends State<MyLogin> {
   final TextEditingController _ctrlEmail = TextEditingController();
   final TextEditingController _ctrlPass = TextEditingController();
-  bool _eye = true; //Variável de controle para mostrar senha
+  bool _eye = true;
 
-  //Variáveis que simularão dados do Banco de Dados
   String emailAccess = "marketpro85@gmail.com";
-  String passAccess = "1234senha";
-  
-  //Função para verificar login e senha
+  String passAccess = "senha1234";
+
+  String _emailFill = "";
+  String _passFill = "";
+
   void _validaLogin(String email, String pass) {
-    if(email == emailAccess && pass == passAccess) {
-      showDialog(
-        context: context,
-        builder: (context) => MyAlertOk()
-      );
+    if(email.isEmpty){
+      setState(() {
+        _emailFill = "Preencha este campo";
+      });
+    } else {
+      setState(() {
+        _emailFill = "";
+      });
+    }
+    if(pass.isEmpty){
+      setState(() {
+        _passFill = "Preencha este campo";
+      });
+    } else {
+      setState(() {
+        _passFill = "";
+      });
+    }
+
+    if (email.isNotEmpty && pass.isNotEmpty) {
+      if(email == emailAccess && pass == passAccess) {
+        showDialog(
+          context: context,
+          builder: (context) => MyAlertOk()
+          );
     } else {
       showDialog(
         context: context,
         builder: (context) => MyAlertFail()
-      );
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-        centerTitle: true
-      ),
+      appBar: AppBar(title: const Text("Login"), centerTitle: true),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Container(
-              width: 150,
-              child: Image.asset("lib/img/logo.gif")
+              width: 200,
+              child: Image.asset("lib/img/logo.png"),
             ),
-            Padding(
-              padding: const EdgeInsets.all(30),
+            Container(
+              padding: EdgeInsets.only(left: 50, right: 50),
               child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: "Digite seu email",
-                  labelText: "Email"
-                ),
-                style: const TextStyle(
-                  fontSize: 24
-                ),
-                controller: _ctrlEmail
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
-              child: TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    icon: (_eye) ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
-                    onPressed: (){
-                      setState(() {
-                        _eye = !_eye;
-                      });
-                    },
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Digite seu email",
+                    helperText: _emailFill,
+                    helperStyle: TextStyle(
+                      color: Colors.red
+                    )
                   ),
-                  labelText: "Senha",
-                  hintText: "Digite sua senha"
-                ),
-                style: const TextStyle(
-                  fontSize: 24
-                ),
-                controller: _ctrlPass,
-                obscureText: _eye
-              ),
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 22),
+                  controller: _ctrlEmail),
+            ),
+            Container(
+              padding:
+                  EdgeInsets.only(left: 50, right: 50, top: 30, bottom: 30),
+              child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Senha",
+                      hintText: "Digite sua senha",
+                      helperText: _passFill,
+                      helperStyle: TextStyle(
+                        color: Colors.red
+                      ),
+                      suffixIcon: IconButton(
+                        icon: (_eye)
+                            ? Icon(Icons.visibility)
+                            : Icon(Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _eye = !_eye;
+                          });
+                        },
+                      )),
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 22),
+                  obscureText: _eye,
+                  controller: _ctrlPass),
             ),
             ElevatedButton(
-              onPressed: (){
-                _validaLogin(_ctrlEmail.text, _ctrlPass.text);
-              },
-              child: Container(
-                padding: const EdgeInsets.all(10),
-                width: 200,
-                child: const Text(
-                  "Login",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24
-                  ),
-                ),
-              )
-            )
+                onPressed: () {
+                  _validaLogin(_ctrlEmail.text, _ctrlPass.text);
+                },
+                child: Container(
+                    padding: EdgeInsets.all(20),
+                    width: 150,
+                    child: Text(
+                      "Login",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 22),
+                    )))
           ],
         ),
       ),
@@ -132,7 +142,7 @@ class _MyLoginState extends State<MyLogin> {
   }
 }
 
-///////////////// Alert Dialog - Ok ////////////////////
+//////////////// AlertDialog - Ok ////////////////
 
 class MyAlertOk extends StatelessWidget {
   const MyAlertOk({Key? key}) : super(key: key);
@@ -140,41 +150,44 @@ class MyAlertOk extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-            content: Text("Login efetuado com sucesso!"),
-            actions: <Widget>[
-              ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MyOtherScreen()
-                      )
-                    );
-                },
-                child: Text("Beleza")
-              ),
-            ],
-          );
-    }
+      content: Text("Login realizado com sucesso!"),
+      title: Text("App 2 - QI"),
+      actions: <Widget>[
+        ElevatedButton(
+          onPressed: (){
+            //Seremos direcionados para outra tela
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MyOtherScreen()
+              )
+            );
+          },
+          child: Text("Ir para home")
+        )
+      ],
+    );
   }
+}
 
-///////////////// Alert Dialog - Fail ////////////////////
+///////////////// AlertDialog - Fail //////////////////
 
-  class MyAlertFail extends StatelessWidget {
+class MyAlertFail extends StatelessWidget {
   const MyAlertFail({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-            content: Text("Acesso negado!"),
-            actions: <Widget>[
-              ElevatedButton(
-                onPressed: (){
-                  Navigator.of(context).pop();
-                },
-                child: Text("Tentar novamente")
-              ),
-            ],
-          );
-    }
+      title: Text("App 2 - QI"),
+      content: Text("Email ou senha inválidos."),
+      actions: <Widget>[
+        ElevatedButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          },
+          child: Text("Tentar novamente")
+        )
+      ],
+    );
   }
+}
